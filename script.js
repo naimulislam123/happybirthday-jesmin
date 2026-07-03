0d1746dd6e88ba9cc535d81eac462bab36a0ee13
naimulislam123 <namul660@gmail.com>
2026-07-03 22:52:19 +0600

    Refactor script.js to add event listeners and styles

--- script.js
+++ script.js
@@ -1,140 +1,15 @@
-*{
-margin:0;
-padding:0;
-box-sizing:border-box;
-font-family:Poppins,sans-serif;
-}
+const startBtn = document.getElementById("startBtn");
+const scene1 = document.getElementById("scene1");
+const scene2 = document.getElementById("scene2");
 
-body{
+const music = document.getElementById("bgMusic");
 
-background:linear-gradient(135deg,#ff9ecb,#ffd6ec,#fff);
+startBtn.onclick = () => {
 
-height:100vh;
+scene1.classList.remove("active");
 
-overflow:hidden;
+scene2.classList.add("active");
 
-display:flex;
+music.play();
 
-justify-content:center;
-
-align-items:center;
-
-}
-
-.scene{
-
-position:absolute;
-
-width:100%;
-
-height:100%;
-
-display:none;
-
-justify-content:center;
-
-align-items:center;
-
-flex-direction:column;
-
-text-align:center;
-
-}
-
-.active{
-
-display:flex;
-
-}
-
-.title{
-
-font-size:34px;
-
-width:80%;
-
-color:#ff2d75;
-
-animation:fadeIn 2s ease;
-
-}
-
-#startBtn{
-
-margin-top:40px;
-
-padding:18px 45px;
-
-font-size:22px;
-
-border:none;
-
-border-radius:40px;
-
-background:#ff4d88;
-
-color:white;
-
-cursor:pointer;
-
-box-shadow:0 0 25px #ff7ca7;
-
-transition:.3s;
-
-}
-
-#startBtn:hover{
-
-transform:scale(1.1);
-
-}
-
-#giftBox{
-
-font-size:150px;
-
-cursor:pointer;
-
-animation:bounce 1.4s infinite;
-
-}
-
-p{
-
-font-size:25px;
-
-color:#ff2d75;
-
-margin-top:20px;
-
-}
-
-@keyframes bounce{
-
-0%{transform:translateY(0);}
-
-50%{transform:translateY(-20px);}
-
-100%{transform:translateY(0);}
-
-}
-
-@keyframes fadeIn{
-
-from{
-
-opacity:0;
-
-transform:translateY(40px);
-
-}
-
-to{
-
-opacity:1;
-
-transform:translateY(0);
-
-}
-
-}
+};
