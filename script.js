98a7ca83f41c88f4ce600a1a48944ea74fdc394b
naimulislam123 <namul660@gmail.com>
2026-07-03 23:11:13 +0600

    Refactor start button event handling
    Refactor start button click handler to use addEventListener and handle music playback.

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 const startBtn = document.getElementById("startBtn");
 const scene1 = document.getElementById("scene1");
 const scene2 = document.getElementById("scene2");
-
 const music = document.getElementById("bgMusic");
 
-startBtn.onclick = () => {
+startBtn.addEventListener("click", () => {
+  scene1.classList.remove("active");
+  scene2.classList.add("active");
 
-scene1.classList.remove("active");
-
-scene2.classList.add("active");
-
-music.play();
-
-};
+  if (music) {
+    music.play().catch(() => {});
+  }
+});
